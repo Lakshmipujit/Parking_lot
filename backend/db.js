f9dab3d308f75b48b7cd350ed4f715773e653bda
Lakshmipujit <lakshmipujithachaganti05@gmail.com>
2026-07-09 20:30:39 +0530

    Add Aiven MySQL SSL connection

--- backend/db.js
+++ backend/db.js
@@ -2,13 +2,16 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
